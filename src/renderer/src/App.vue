f0187f44b687130d064612283eaf05d903acdbcb
gxstar <gengxing123@qq.com>
2025-07-26 12:20:35 +0800

    完成所有基础配置

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold text-blue-600">照片管理工具</h1>
+    <p class="text-gray-700 mt-2">使用Tailwind CSS样式化你的界面</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
